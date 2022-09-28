e92f5c8f6f54852600e1331e327b599a00567938
bjsaimani <45102237+bjsaimani@users.noreply.github.com>
2022-09-28 19:05:13 +0530

    successfull sign up

--- signUp/signUp.js
+++ signUp/signUp.js
@@ -7,4 +7,30 @@ document.getElementById('main_container').addEventListener('click',(e)=>{
     if(e.target.id==='password_input'){e.target.value=''}
 
     // sign button click evet handling
+    if(e.target.id==='signUp_btn'){
+        e.preventDefault();
+        const name= document.getElementById('name_input').value;
+        const email= document.getElementById('email_input').value;
+        const number= document.getElementById('number_input').value;
+        const password=document.getElementById('password_input').value;
+
+        if(name && email && number && password){
+            axios.post('http://localhost:5000/user/sign_up',{name,email,number,password})
+            .then((result)=>{
+                console.log(result);
+                if(result.status ===203){
+                    window.alert(result.data.msg)
+                }
+                else{
+                    window.alert(result.data.msg);
+                }
+            })
+            .catch(err => console.log(err));
+        }else{
+            window.alert('please enter the all fields');
+        }
+        console.log('clicked')
+
+    }
+
 })
