242d23d810b98d1055f93ba5256fbe24e8c17699
bjsaimani <45102237+bjsaimani@users.noreply.github.com>
2022-09-28 15:31:48 +0530

    init

---
+++ signUp/signUp.js
@@ -0,0 +1,10 @@
+
+const name = document.getElementById('name_input');
+
+document.getElementById('main_container').addEventListener('click',(e)=>{
+
+    // removing the existing password contect from field
+    if(e.target.id==='password_input'){e.target.value=''}
+
+    // sign button click evet handling
+})
