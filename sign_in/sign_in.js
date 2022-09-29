9d274f5393d39e30ff555b39ffd24dc9ea66eddd
bjsaimani <45102237+bjsaimani@users.noreply.github.com>
2022-09-29 14:47:26 +0530

    creating fchat interface

--- sign_in/sign_in.js
+++ sign_in/sign_in.js
@@ -1,12 +1,19 @@
 
-document.getElementById('signIn_btn').addEventListener('click',()=>{
-    const email= document.getElementById('email_input');
-    const password=document.getElementById('password_input');
+document.getElementById('signIn_btn').addEventListener('click',(e)=>{
+    e.preventDefault();
+    const email= document.getElementById('email_input').value;
+    const password=document.getElementById('password_input').value;
 
     if(email && password){
         axios.post('http://localhost:5000/user/sign_in',{email,password})
         .then(result=>{
-            console.log(result);
+            console.log(result)
+            if(result.status===200){
+                window.alert(result.data.msg);
+                localStorage.setItem('token',result.data.token);
+            }else{
+                window.alert(result.data.msg)
+            }
         })
         .catch(err =>console.log(err));
     }else{
